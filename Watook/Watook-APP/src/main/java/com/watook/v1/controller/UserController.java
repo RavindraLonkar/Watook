380724b7d83e74c40e61e184522fa31e6c9a2d04
RavindraLonkar <ravi.lonkar888@gmail.com>
2017-10-11 20:11:04 +0530

    auth request added

--- Watook/Watook-APP/src/main/java/com/watook/v1/controller/UserController.java
+++ Watook/Watook-APP/src/main/java/com/watook/v1/controller/UserController.java
@@ -2,8 +2,11 @@ package com.watook.v1.controller;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.core.env.Environment;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -31,12 +34,36 @@ public class UserController {
 
 	private static final Logger logger = Logger.getLogger(UserController.class);
 		
+	@RequestMapping(value = "/auth", method = RequestMethod.GET)
+	public Response getUserAuth(HttpServletRequest request) {
+		
+		try{
+			String applicationId = request.getParameter("applicationId");
+			String fbToken = request.getParameter("fbToken");
+			
+			if(fbToken.equals("") || fbToken == null)
+				return new Response(CommonConstants.FAIL, null, CommonUserMessages.AUTH_FAIL);
+			
+			FBAuthResponse fBAuthResponse = fbAuthentication(fbToken);
+			
+			if(fBAuthResponse.getId() == null || !fBAuthResponse.getId().equals(applicationId))
+				return new Response(CommonConstants.FAIL, null, CommonUserMessages.AUTH_FAIL);
+			
+			String token = WatookToken.encrypt(applicationId, environment.getRequiredProperty("ENCY_USER_KEY"));
+			
+			return new Response(CommonConstants.SUCCESS, token, CommonUserMessages.AUTH_SUCCESS);	
+			
+		}catch(Exception e){
+			return new Response(CommonConstants.FAIL, null, CommonUserMessages.AUTH_FAIL);
+		}
+		
+	}
+	
 	@RequestMapping(value = "/list", method = RequestMethod.GET)
 	public Response getUserList() {
 		Response response = null;
 								
-		try{
-						
+		try{						
 			List<User> userList = userService.getUserList();
 			if (userList.isEmpty()) {
 				response = new Response(CommonConstants.FAIL, null, CommonConstants.SYSTEM_ERROR);
@@ -54,22 +81,12 @@ public class UserController {
 	@RequestMapping(value = "/save", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
 	public Response save(@RequestBody User user) {
 		Response response = null;
-		
-		if(user.getFbToken().equals("") || user.getFbToken() == null)
-			return new Response(CommonConstants.FAIL, null, CommonUserMessages.AUTH_FAIL);
-		
-		FBAuthResponse fBAuthResponse = fbAuthentication(user);
-		
-		if(fBAuthResponse.getId() == null || !fBAuthResponse.getId().equals(user.getApplicationId()))
-			return new Response(CommonConstants.FAIL, null, CommonUserMessages.AUTH_FAIL);
-				
 		try{
 						
 		    User userCreated = userService.save(user);
 			if (userCreated.getUserId() == null) {
 				response = new Response(CommonConstants.FAIL, userCreated, CommonConstants.SYSTEM_ERROR);
 			} else {		
-				userCreated.setToken(WatookToken.encrypt(user.getFbId(), environment.getRequiredProperty("ENCY_USER_KEY")));
 				response = new Response(CommonConstants.SUCCESS, userCreated, CommonUserMessages.USER_SAVED);
 			}
 		}catch(Exception e){
@@ -77,31 +94,13 @@ public class UserController {
 		}		
 		return response;
 	}
-	
-	@RequestMapping(value = "/update", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
-	public Response update(@RequestBody User user) {
-		Response response = null;				
-		try{
-						
-		    User userCreated = userService.save(user);
-			if (userCreated.getUserId() == null) {
-				response = new Response(CommonConstants.FAIL, userCreated, CommonConstants.SYSTEM_ERROR);
-			} else {		
-				userCreated.setToken(WatookToken.encrypt(user.getFbId(), environment.getRequiredProperty("ENCY_USER_KEY")));
-				response = new Response(CommonConstants.SUCCESS, userCreated, CommonUserMessages.USER_SAVED);
-			}
-		}catch(Exception e){
-			logger.info("Error : " + e);
-		}		
-		return response;
-	}
-	
-	private FBAuthResponse fbAuthentication(User user){
+		
+	private FBAuthResponse fbAuthentication(String fbToken){
 		FBAuthResponse fBAuthResponse = null;
 		try {
 						
 			RestTemplate restTemplate = new RestTemplate();
-			String fbAuthUrl = "https://graph.facebook.com/app/?access_token=" + user.getFbToken();			
+			String fbAuthUrl = "https://graph.facebook.com/app/?access_token=" + fbToken;			
 			fBAuthResponse = restTemplate.getForObject(fbAuthUrl, FBAuthResponse.class);
 			fBAuthResponse.setIsAuthSuccess(true);
 		} catch (Exception e) {
