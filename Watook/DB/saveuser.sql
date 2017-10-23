d0ac9dd6fa8fff4ab8e6d88c43bdb928950ad19a
RavindraLonkar <ravi.lonkar888@gmail.com>
2017-10-23 15:56:58 +0530

    SP changes for work info columns

--- Watook/DB/saveuser.sql
+++ Watook/DB/saveuser.sql
@@ -1,24 +1,26 @@
--- Function: public.saveuser(text)
-
+/***********************************************************************************************
+AUTHOR : Namdev Arade
+select public.saveuser('{"fbid":"12366","lastname":"Avffffinash","workemployer":"Extrapreneurs","statusinfo":"401","isactive":"1"}')
+***********************************************************************************************/
 -- DROP FUNCTION public.saveuser(text);
 
 CREATE OR REPLACE FUNCTION public.saveuser(userjson text)
   RETURNS character varying AS
 $BODY$
-  DECLARE result character varying; 
+	 DECLARE result character varying; 
  BEGIN
 
- 
+	
     
-      INSERT INTO txn_user(fbid,firstname,middlename,lastname,dob,genderid,contactmobile,contactmobile2,emailid,advertiseid,aboutyou,workinfo,statusinfo,fbimages,
+      INSERT INTO txn_user(fbid,firstname,middlename,lastname,dob,genderid,contactmobile,contactmobile2,emailid,advertiseid,aboutyou,workemployer,worklocation,workposition,statusinfo,fbimages,
       profileimage,isactive,createdby,createddate,lastmodifiedby,lastmodifieddate)
-      SELECT fbid,firstname,middlename,lastname,dob,genderid,contactmobile,contactmobile2,emailid,advertiseid,aboutyou,workinfo,statusinfo,fbimages,
+      SELECT fbid,firstname,middlename,lastname,dob,genderid,contactmobile,contactmobile2,emailid,advertiseid,aboutyou,workemployer,worklocation,workposition,statusinfo,fbimages,
       profileimage,isactive,1,now(),1,now() FROM json_populate_record(null::txn_user, userjson::json)
       WHERE NOT EXISTS (select 1 from txn_user where fbid = (select userJson::json->>'fbid') limit 1)
       RETURNING userid into result;
 
      --raise notice 'value%',  ret;
- 
+	
       if(result IS NULL)then
        UPDATE txn_user 
           SET firstname = (select userJson::json->>'firstname')
@@ -31,7 +33,9 @@ $BODY$
              ,emailid = (select userJson::json->>'emailid')
              ,advertiseid = (select userJson::json->>'advertiseid')  
              ,aboutyou = (select userJson::json->>'aboutyou')
-             ,workinfo = (select userJson::json->>'workinfo')
+	         ,workemployer = (select userJson::json->>'workemployer')
+	         ,worklocation = (select userJson::json->>'worklocation')
+             ,workposition = (select userJson::json->>'workposition')
              ,statusinfo = (select cast(userJson::json->>'statusinfo' as int))
              ,fbimages = (select userJson::json->>'fbimages')
              ,profileimage = (select userJson::json->>'profileimage')
@@ -47,4 +51,6 @@ $BODY$
   LANGUAGE plpgsql VOLATILE
   COST 100;
 ALTER FUNCTION public.saveuser(text)
-  OWNER TO "Watook";
+  OWNER TO postgres;
+
+
